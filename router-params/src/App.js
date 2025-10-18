e2372581d30104fea49923c482ae5bbc4871a4f3
Alice Kim <alice.kim@qualia.com>
2025-10-18 12:02:22 -0700

    replace

--- router-params/src/App.js
+++ router-params/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+
+export default function App() {
+  //Insert router, links here
+}
+
+function Child() {
+
+  // Below this comment, there's one major key script missing
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>You Selected:<span>{id}</span></h3>
     </div>
   );
 }
-
-export default App;
